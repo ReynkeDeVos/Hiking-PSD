15178524b90ff6f280f041f92c35c52723ba5577
janghw33 <janghw33@gmail.com>
2024-06-10 10:58:30 +0200

    group 4-5, 6 setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Group4_5 from './pages/Group4-5';
+import Group6 from './pages/Group6';
 
 function App() {
-  return <></>;
+  return <>
+  <Group4_5 />
+  <Group6 />
+  </>;
 }
 
 export default App;
---
+++ src/pages/Group4-5.jsx
@@ -0,0 +1,7 @@
+import { react } from 'react';
+
+const Group4_5 = () => {
+  return <div>Group4_5</div>;
+}
+
+export default Group4_5;
