5e6c3f9772a0414039e670978a0288c2d9b2f406
Vladimir <gerov.vladimir@gmail.com>
2024-06-10 16:00:14 +0200

    first version of section 8,9

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 import Group00_navbar from './components/Group00_navbar';
 import Group3_4 from './components/Group3_4';
 import Group5 from './components/Group5';
-// import Footer from './components/footerSection';
-import Footer from './components/FooterSection.jsx';
+import Footer from './components/Footer';
+import Subscribe from './components/Subscribe';
 
 function App() {
   return (
@@ -11,8 +11,10 @@ function App() {
       <Group00_navbar />
     <Group3_4 />
   <Group5 />
+  <Subscribe />
+  <Footer />
     </>
   );
-}
+};
 
 export default App;
