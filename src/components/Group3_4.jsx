49940a744577633a3123c6f76eb3f04d29689d1a
janghw33 <janghw33@gmail.com>
2024-06-10 11:35:38 +0200

    group4

--- src/components/Group3_4.jsx
+++ src/components/Group3_4.jsx
@@ -3,15 +3,39 @@ import { react } from 'react';
 const Group3_4 = () => {
   return (
     <>
-      <div>Group3_4</div>
+      
 
-      <low>
-        <image src="../src/assets/Group4/04_01">
-        </image>
-        <p>
-
-        </p>
-      </low>
+      <div className="flex items-center">
+        <img src="../src/assets/Group03/03_01.png" className="w-1/2 mr-4" />
+        <aside>
+          <h1>
+            Discount up to 50% All Excursions
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Necessitatibus corrupti ex sit aut laboriosam quasi 
+            alias sint magnam a nam, dolor et quia deserunt
+            ut optio ea perferendis facilis delectus.
+          </p>
+        </aside>
+      </div>
+      <div className="flex items-center">
+        <aside  className="w-1/2 mr-4">
+          <h1>
+            Januarys' Promo: <br/>
+            Buy 1 Get 1 Free!
+          </h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Asperiores architecto voluptatum non aperiam esse,
+              tenetur molestiae saepe harum suscipit excepturi
+              provident mollitia, accusantium id veritatis laborum
+              dolorem ducimus at! Natus!
+            </p>
+          </aside>
+        <img src="../src/assets/Group04/04_01.png" className="w-1/2" />
+      </div>
+      
     </>
   )
 }
