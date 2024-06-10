c99b2a805cd630e9101705beebe836f5f77d4dd1
janghw33 <janghw33@gmail.com>
2024-06-10 16:00:14 +0200

    final group5 time over

--- src/components/Group5.jsx
+++ src/components/Group5.jsx
@@ -1,8 +1,10 @@
 import { react } from 'react';
+import shadowImage from '../assets/Group02/02_shadow.png';
 
 const Group5 = () => {
   return (
     <>
+    
       <div className="group5-bg relative w-full " style={{
         width: '100%',
         height: '765px',
@@ -11,6 +13,8 @@ const Group5 = () => {
         backgroundColor: '#7f7770'
         
       }}>
+        
+
 
       <div className="before mt-40 bg-cover bg-no-repeat" style={{
         content: "",
@@ -18,17 +22,17 @@ const Group5 = () => {
         height: "15%",
         backgroundImage: "url(../src/assets/Group05/tornpaper_at_the_top.png)"
       }}></div>
+      
       <div className="before bg-cover bg-no-repeat w-[100%] h-[15%]" style={{
         content: "",
         marginTop: "600px",
         backgroundImage: "url(../src/assets/Group05/tornpaper_at_the_bottom.png)"
       }}></div>
-      <div className="relative h-[40%] w-[40%] before bg-cover transform scaleX-[-1]" style={{
-        bottom: "600px",
-        backgroundImage: "url(../src/assets/Group05/leaf_shadow.png)"
-      }}></div>
-      
-
+      <img
+        src={shadowImage}
+        alt="Shadow"
+        className="-translate-x-1/10 absolute bottom-20 left-10 h-1/2 w-1/2 scale-x-[-1] transform opacity-50"
+      />
     </div>
     </>
   )
