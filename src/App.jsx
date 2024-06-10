2d8f276e11b8ee064126aff0fb42205125f4e29e
Vladimir <gerov.vladimir@gmail.com>
2024-06-10 11:48:32 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
+// import Footer from './components/footerSection';
+import Footer from './components/FooterSection.jsx';
 
 function App() {
   return <></>;
-}
+};
 
 export default App;
---
+++ src/components/FooterSection.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return <footer>
+        <div>
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, corrupti?</h1>
+        </div>
+    </footer>;
+};
+
+// export default Footer;
