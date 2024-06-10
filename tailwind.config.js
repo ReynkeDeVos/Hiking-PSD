741400a16207052448f041446498f364a5c61a00
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-06-10 13:10:09 +0200

    added hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,24 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Open Sans', 'sans-serif'],
+      colors: {
+        deb37e: '#deb37e',
       },
+      fontSize: {
+        92: '92px',
+      },
+      fontFamily: {
+        OpenSans: ['Open Sans', 'sans-serif'],
+      },
+      filter: {},
+      dropShadow: {
+        custom: '2.575px -4.286px 2.5px rgba(84,82,79,0.22)',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      filter: ['responsive'],
     },
   },
   plugins: [],
