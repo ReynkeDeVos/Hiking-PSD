f36c1152fbb9231a4aa041303215cd54d9312110
janghw33 <janghw33@gmail.com>
2024-06-10 11:16:34 +0200

    444555

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import Group00_navbar from './components/Group00_navbar';
-import Group4_5 from './components/Group4-5';
-import Group6 from './components/Group6';
+import Group3_4 from './components/Group3_4';
+import Group5 from './components/Group5';
 
 function App() {
   return (
     <>
       <Group00_navbar />
-    <Group4_5 />
-  <Group6 />
+    <Group3_4 />
+  <Group5 />
     </>
   );
 }
---
+++ src/components/Group3_4.jsx
@@ -0,0 +1,19 @@
+import { react } from 'react';
+
+const Group3_4 = () => {
+  return (
+    <>
+      <div>Group3_4</div>
+
+      <low>
+        <image src="../src/assets/Group4/04_01">
+        </image>
+        <p>
+
+        </p>
+      </low>
+    </>
+  )
+}
+
+export default Group3_4;
