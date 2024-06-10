3ea2d370fead932d610a4c9a5691362de5534d84
janghw33 <janghw33@gmail.com>
2024-06-10 14:18:15 +0200

    group5-tornpaper

--- src/components/Group5.jsx
+++ src/components/Group5.jsx
@@ -3,9 +3,22 @@ import { react } from 'react';
 const Group5 = () => {
   return (
     <>
-    <div className='group5-bg'>
-      braun
-
+    <div className="group5-bg relative w-full h-765 opacity-80 bg-gray-700" style={{
+    width: '100%',
+    height: '765px',
+    opacity: 0.8,
+    position: 'relative',
+    backgroundColor: '#7f7770',
+  }}>
+      <div className="before left-0 block mt-60 w-full h-15 mb-100 bg-cover bg-no-repeat" style={{
+    content: "",
+    left: 0,
+    display: "block",
+    marginTop: "60px",
+    width: "100%",
+    height: "15%",
+    backgroundImage: "url(../src/assets/Group05/tornpaper_at_the_top.png)"
+  }}></div>
     </div>
     </>
   )
